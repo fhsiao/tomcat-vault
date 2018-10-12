e1d507f83ce02377189e47bd851eca15d2525c5c
fhsiao <frank.hsiao@hotmail.com>
2018-10-11 18:46:45 -0600

    VIDOPSTECH-8255: Add junit test classes

---
+++ src/test/java/com/charter/tomcatvault/PROPTest.java
@@ -0,0 +1,166 @@
+package com.charter.tomcatvault;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PROPTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void addResource() {
+    }
+
+    @Test
+    public void getResourceMap() {
+    }
+
+    @Test
+    public void getResourceSize() {
+    }
+
+    @Test
+    public void getResourceToken() {
+    }
+
+    @Test
+    public void setResourceToken() {
+    }
+
+    @Test
+    public void getResourcePoto() {
+    }
+
+    @Test
+    public void setResourcePoto() {
+    }
+
+    @Test
+    public void getResourceHost() {
+    }
+
+    @Test
+    public void setResourceHost() {
+    }
+
+    @Test
+    public void getResourcePort() {
+    }
+
+    @Test
+    public void setResourcePort() {
+    }
+
+    @Test
+    public void getResourcePath() {
+    }
+
+    @Test
+    public void setResourcePath() {
+    }
+
+    @Test
+    public void getResourceUser() {
+    }
+
+    @Test
+    public void setResourceUser() {
+    }
+
+    @Test
+    public void getResourceOpenTimeout() {
+    }
+
+    @Test
+    public void setResourceOpenTimeout() {
+    }
+
+    @Test
+    public void getResourceReadTimeout() {
+    }
+
+    @Test
+    public void setResourceReadTimeout() {
+    }
+
+    @Test
+    public void updateKVWarning() {
+    }
+
+    @Test
+    public void getGlobalSize() {
+    }
+
+    @Test
+    public void getGlobalToken() {
+    }
+
+    @Test
+    public void setGlobalToken() {
+    }
+
+    @Test
+    public void getGlobalPoto() {
+    }
+
+    @Test
+    public void setGlobalPoto() {
+    }
+
+    @Test
+    public void getGlobalHost() {
+    }
+
+    @Test
+    public void setGlobalHost() {
+    }
+
+    @Test
+    public void getGlobalPort() {
+    }
+
+    @Test
+    public void setGlobalPort() {
+    }
+
+    @Test
+    public void getGlobalPath() {
+    }
+
+    @Test
+    public void setGlobalPath() {
+    }
+
+    @Test
+    public void getGlobalUser() {
+    }
+
+    @Test
+    public void setGlobalUser() {
+    }
+
+    @Test
+    public void getGlobalOpenTimeout() {
+    }
+
+    @Test
+    public void setGlobalOpenTimeout() {
+    }
+
+    @Test
+    public void getGlobalReadTimeout() {
+    }
+
+    @Test
+    public void setGlobalReadTimeout() {
+    }
+}
---
+++ src/test/java/com/charter/tomcatvault/VaultClientTest.java
@@ -0,0 +1,26 @@
+package com.charter.tomcatvault;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class VaultClientTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getVault() {
+    }
+
+    @Test
+    public void getVault1() {
+    }
+}
